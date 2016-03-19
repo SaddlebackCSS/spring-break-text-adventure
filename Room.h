05275ea0b05683b211aacb80736e894eecea8600
Zach Allnutt <Zach Allnutt>
2016-03-18 21:59:11 -0700

    19 Added the basic game mechanics, to be fleshed out a bit more soon

--- Room.h
+++ Room.h
@@ -1,26 +1,17 @@
-#ifndef SPRING_BREAK_TEXT_ADVENTURE_ROOM_H
-#define SPRING_BREAK_TEXT_ADVENTURE_ROOM_H
-
+#pragma once
 #include <string>
-#include <map>
 
 class Room
 {
 public:
-    Room();
-    Room(std::string desc, std::string N, std::string S, std::string E, std::string W);
-    ~Room();
-
-    std::string GetDesc()const;
-
-private:
-    std::string description;
-    std::map<std::string, std::string> linked;
-    std::string north;
-    std::string south;
-    std::string east;
-    std::string west;
+	std::string northRoom;
+	std::string eastRoom;
+	std::string southRoom;
+	std::string westRoom;
+	std::string roomName;
+	std::string roomDesc;
+	Room(std::string name, std::string desc, std::string north = "none", std::string east = "none", std::string south = "none", std::string west = "none");
+	Room();
+	~Room();
 };
 
-
-#endif //SPRING_BREAK_TEXT_ADVENTURE_ROOM_H
